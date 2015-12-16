436d1931a947a6f40feadffcd4204f054b264f81
Shamash2014 <warp.buddhist@gmail.com>
2015-12-17 00:53:08 +0200

    Add new exercise ruby. Too much recursion

--- ruby/exercise1.rb
+++ ruby/exercise1.rb
@@ -13,3 +13,29 @@ end
 assert { squares_max(3, 4, 5) == 41 }
 assert { squares_max(1, 2, 3) == 13 }
 assert { squares_max(1, 2, 10) != 24 }
+
+#1.7
+def average x, y
+ (x + y) / 2.0
+end
+
+def better_guess x, y
+  average y, (x / y)
+end
+
+def good_enough? x, y
+ (x ** 2 - y ** 2) < 0.001
+end
+
+def square_iter x, y
+ if good_enough? x, y
+   y
+  else
+    square_iter x, better_guess(x, y)
+ end
+end
+
+def sqrt x
+  square_iter x, 1.0
+end
+puts sqrt 25
